4bfdbd8456199d164e5626463dad011d9ad96e6c
shirazisam <shiraz.isam@hotmail.com>
2024-06-17 21:36:15 +0200

    - Spring Service added with Random book generator - Endpoint book controller added

--- src/main/java/com/bnp/kata/infrastructure/adapter/BookDiscountService.java
+++ src/main/java/com/bnp/kata/infrastructure/adapter/BookDiscountService.java
@@ -3,7 +3,41 @@ package com.bnp.kata.infrastructure.adapter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 @Slf4j
 @Service
 public class BookDiscountService {
+
+    public void calculateBookDiscounts(String nrBooks) {
+        log.info("Calculating discounts for {} books", nrBooks);
+        int n = Integer.parseInt(nrBooks);
+        List<String> books = generateRandomBookList(n);
+        books.forEach(System.out::println);
+    }
+
+    /**
+     * Generate a random, sorted list of Book1..Book5
+     * @param numberOfBooks the number of books in the shopping cart
+     * @return the sorted list of random books
+     */
+    List<String> generateRandomBookList(int numberOfBooks) {
+        return IntStream.range(0,numberOfBooks)
+                .boxed()
+                .map(c -> "Book " + getRandomNumberClosed(1,5))
+                .sorted()
+                .toList();
+    }
+
+    /**
+     * Generate a random number
+     * @param min lower boundary
+     * @param max upper inclusive boundary
+     * @return a random number in the range
+     */
+    public int getRandomNumberClosed(int min, int max) {
+        return (int) ((Math.random() * (max - min +1)) + min);
+    }
+
 }
---
+++ src/main/java/com/bnp/kata/web/BookController.java
@@ -0,0 +1,25 @@
+package com.bnp.kata.web;
+
+import com.bnp.kata.infrastructure.adapter.BookDiscountService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/books")
+public class BookController {
+
+    private final BookDiscountService bookDiscountService;
+
+    public BookController(BookDiscountService bookDiscountService) {
+        this.bookDiscountService = bookDiscountService;
+    }
+
+    @GetMapping
+    public String calculateDiscount(@RequestParam("nrbooks") String nrBooks) {
+        bookDiscountService.calculateBookDiscounts(nrBooks);
+        return nrBooks;
+    }
+
+}
