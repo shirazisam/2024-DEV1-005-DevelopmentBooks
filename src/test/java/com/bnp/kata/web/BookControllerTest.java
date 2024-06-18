a742b1731f554910a60fc46dd06b0a4548a8e5cd
shirazisam <shiraz.isam@hotmail.com>
2024-06-18 20:57:04 +0200

    test added for invalid 'nrbooks' query parameter

--- src/test/java/com/bnp/kata/web/BookControllerTest.java
+++ src/test/java/com/bnp/kata/web/BookControllerTest.java
@@ -1,11 +1,13 @@
 package com.bnp.kata.web;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.server.ResponseStatusException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -25,6 +27,17 @@ class BookControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.bookPrice").value("50"));
+
+    }
+
+    @Test
+    void calculateDiscountNrBooksNaN() throws Exception {
+        mvc.perform(get("/books?nrbooks=hello"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> Assertions.assertInstanceOf(ResponseStatusException.class, result.getResolvedException()))
+                .andExpect(result -> Assertions.assertEquals("400 BAD_REQUEST \"'nrbooks' must be a number\"",
+                        result.getResolvedException().getMessage()));
 
     }
 }
